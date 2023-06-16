c90785580b4f7e63722b71e3fb1a9985f4da1a1a
Shimaa Adel <shimaa@troylab.org>
2023-06-16 12:06:04 +0300

    WIP testing

--- jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  preset: "react-scripts",
-  // Add your additional configuration options here
-};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import axios from "axios";
 import App from "./App";
-
-jest.mock("axios");
 
 test("renders the App component", () => {
   render(<App />);
