ae0b3b2dfe364697eeb9f14066eb13d337534d45
Shimaa Adel <shimaa@troylab.org>
2023-06-16 11:23:44 +0300

    WIP testing

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  preset: "react-scripts",
+  // Add your additional configuration options here
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import axios from "axios";
+import App from "./App";
 
-test('renders learn react link', () => {
+jest.mock("axios");
+
+test("renders the App component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Add your assertions here to test the rendered output or behavior
 });
