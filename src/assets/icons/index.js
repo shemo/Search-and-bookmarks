1691d8627fca713efa9b3590fb148920f1705fe0
Shimaa Adel <shimaa@troylab.org>
2023-06-17 14:15:47 +0300

    TIP app requirements + UI enhancements

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,6 @@
 import { FaBoxOpen } from "react-icons/fa";
 import { HiOutlineStar } from "react-icons/hi";
+import { BiGitRepoForked } from "react-icons/bi";
 export const BoxIcon = FaBoxOpen;
 export const StarIcon = HiOutlineStar;
+export const ForkIcon = BiGitRepoForked;
--- src/components/RepositoryItem.js
+++ src/components/RepositoryItem.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { StarIcon } from "../assets/icons";
+import { StarIcon, ForkIcon } from "../assets/icons";
 import BookmarkButton from "./BookmarkButton";
 
 const RepositoryItem = ({ repository, onBookmark, isBookmarked }) => {
-  const { id, name, owner, description, stargazers_count } = repository;
+  const { id, name, owner, description, stargazers_count, forks_count } =
+    repository;
 
   const handleBookmark = () => {
     onBookmark(repository);
@@ -20,7 +21,7 @@ const RepositoryItem = ({ repository, onBookmark, isBookmarked }) => {
 
       <p className="text-gray-700 capitalize break-words">
         {" "}
-        {description?.substring(0, 50)} {description?.length > 50 && ".."}
+        {description?.substring(0, 40)} {description?.length > 40 && ".."}
       </p>
 
       <div className="flex flex-col lg:flex-row  gap-2 items-center justify-between">
@@ -39,10 +40,20 @@ const RepositoryItem = ({ repository, onBookmark, isBookmarked }) => {
             </a>
           </div>
         </div>
-        <p className="flex items-center gap-2 text-gray-600">
-          <StarIcon size={20} />{" "}
-          <span className="font-semibold">{stargazers_count}</span>
-        </p>
+        <div className="flex items-center gap-5">
+          <p className="flex items-center gap-2 text-gray-600">
+            <StarIcon size={20} />{" "}
+            <span className="font-semibold">
+              {stargazers_count} {stargazers_count == 1 ? "start" : "stars"}
+            </span>
+          </p>
+          <p className="flex items-center gap-2 text-gray-600">
+            <ForkIcon size={20} />{" "}
+            <span className="font-semibold">
+              {forks_count} {forks_count == 1 ? "fork" : "forks"}
+            </span>
+          </p>
+        </div>
       </div>
     </li>
   );
