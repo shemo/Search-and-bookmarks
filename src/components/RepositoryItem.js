d69d095cc87b92e6fb8d9cc1bdebe27ba867ce9e
Shimaa Adel <shimaa@troylab.org>
2023-06-17 13:58:15 +0300

    TIP app requirements + UI enhancements

--- src/components/RepositoryItem.js
+++ src/components/RepositoryItem.js
@@ -13,29 +13,29 @@ const RepositoryItem = ({ repository, onBookmark, isBookmarked }) => {
     <li className="flex flex-col gap-3 bg-white rounded p-4 shadow-md relative">
       <BookmarkButton onClick={handleBookmark} isBookmarked={isBookmarked} />
 
-      <h3 className="text-xl font-semibold capitalize">
+      <h3 className="text-xl font-semibold capitalize break-words pe-8">
         {" "}
         {name?.substring(0, 30)} {name?.length > 30 && ".."}
       </h3>
 
-      <p className="text-gray-700 vapitalize">
+      <p className="text-gray-700 capitalize break-words">
         {" "}
         {description?.substring(0, 50)} {description?.length > 50 && ".."}
       </p>
 
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col lg:flex-row  gap-2 items-center justify-between">
         <div className="flex items-center gap-4">
           <a href={owner.html_url} target="_blank" noreferer="true">
             {" "}
             <img
-              className="w-10 h-10 rounded-full"
+              className="w-10 h-10 rounded-full shrink-0"
               src={owner.avatar_url}
               alt={owner.login}
             />
           </a>
           <div className="font-medium dark:text-white">
             <a href={owner.html_url} target="_blank" noreferer="true">
-              {owner.login}
+              {owner.login?.substring(0, 15)} {owner.login?.length > 15 && ".."}
             </a>
           </div>
         </div>
